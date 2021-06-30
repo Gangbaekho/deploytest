c0dc66449df7419c181086d3ef3ae35bfaf8647d
kimjinsoo <roorooro9933@gmail.com>
2021-06-30 13:37:00 +0900

    deply test

---
+++ src/main/java/com/nuit/deploy/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nuit.deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "This is Nuit-Blanche!...";
+    }
+}
